7e831b28aa9a6c605b5ae979bf725e461f3e46cd
Eduardo L. Francisco <eduardolf85@gmail.com>
2016-07-23 12:24:05 -0300

    Customer CRUD views and controller complete.

--- api/controllers/CustomerController.js
+++ api/controllers/CustomerController.js
@@ -29,6 +29,45 @@ module.exports = {
         customer: customer
       });
     });
+  },
+
+  index: function (req, res, next) {
+    Customer.find(req.param('id'), function foundCustomer(err, customers) {
+      if (err) return next(err);
+      //if (!customers) return next();
+
+      res.view({
+        customers: customers
+      });
+    });
+  },
+
+  edit: function (req, res, next) {
+    Customer.findOne(req.param('id'), function foundCustomer(err, customer) {
+      if (err) return next(err);
+      if (!customer) return next();
+
+      res.view({
+        customer: customer
+      });
+    });
+  },
+
+  update: function (req, res, next) {
+    //var id = req.param('id');
+    Customer.update(req.param('id'), req.params.all(), function customerUpdated(err) {
+      if (err) {
+        return res.redirect('/customer/edit/' + req.param('id'));
+      }
+
+      res.redirect('/customer/show/' + req.param('id'));
+    });
+  },
+
+  destroy: function (req, res, next) {
+    Customer.destroy(req.param('id')).exec( function () {
+      res.redirect('/customer');
+    });
   }
 
 };
